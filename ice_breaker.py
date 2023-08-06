889a5b20c1c60a6de6bee2720ea78010d7537269
jjlovaglio <jjlovaglio@gmail.com>
2023-08-06 09:53:57 -0300

    finish lec6 project setup on vscode

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,3 +1,4 @@
+import os
 from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
@@ -5,19 +6,20 @@ from langchain.chains import LLMChain
 
 if __name__ == '__main__':
     print('hello LangChain')
+    print(os.environ['OPENAI_API_KEY'])
 
-    summary_template = """
-        given the information {information} about a person from I want you to create:
-        1. a short summary
-        2. two interesting facts about them
-    """
+    # summary_template = """
+    #     given the information {information} about a person from I want you to create:
+    #     1. a short summary
+    #     2. two interesting facts about them
+    # """
 
-    summary_prompt_template = PromptTemplate(
-        input_variables="information", template=summary_template
-    )
+    # summary_prompt_template = PromptTemplate(
+    #     input_variables="information", template=summary_template
+    # )
 
-    llm = ChatOpenAi(temperature=0, model_name="gpt-3.5-turbo")
+    # llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
-    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    # chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
     
